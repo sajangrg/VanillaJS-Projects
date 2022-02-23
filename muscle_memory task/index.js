8f0eed5be8547bffbcbdf87f4959233ecac848d5
Sajan Gurung <sggrggrg@gmail.com>
2022-02-23 22:15:12 +0545

    completed Chrome Extention and some practive

--- muscle_memory task/index.js
+++ muscle_memory task/index.js
@@ -32,3 +32,20 @@ function multiply() {
     let result = num1 * num2
     document.getElementById("sum-el").textContent = "Sum: " + result;
 }
+
+// Let & Const.
+
+const player = "Per"
+const opponent = "Nick"
+const game = "AmazingFighter"
+let points = 0
+let hasWon = false
+
+points += 100
+hasWon = true
+
+if(hasWon){
+  console.log(`${player} got ${points} points and won the ${game} game!`)
+} else{
+  console.log(`The winner is ${opponent}! ${player} lost the game.`)
+}
