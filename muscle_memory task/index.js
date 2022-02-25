8ed070b85978650afa58bbe796965b2702924dab
Sajan Gurung <sggrggrg@gmail.com>
2022-02-25 23:45:43 +0545

    Muscle memory push

--- muscle_memory task/index.js
+++ muscle_memory task/index.js
@@ -49,3 +49,90 @@ if(hasWon){
 } else{
   console.log(`The winner is ${opponent}! ${player} lost the game.`)
 }
+
+// Log out items in an array
+
+let myCourses = ["Learn CSS Animations", "UI Design Fundamentals", "Intro to Clean Code"]
+
+function listOutCourses(arr){
+  for(i=0; i<arr.length; i++){
+    console.log(arr[i])
+  }
+}
+
+listOutCourses(myCourses)
+
+
+// Save to localStorage
+
+let carBrand = ["Farrari", "Suzuki", "Alto", "Ford"]
+
+// localStorage.setItem('Car Brand', JSON.stringify(carBrand));
+
+let carEntry = JSON.parse(localStorage.getItem("Car Brand"));
+
+console.log(carEntry[3]);
+
+// addEventListener and object in array
+
+let data = [
+  {
+    player: "Jane",
+    score: 52
+  },
+  {
+    player: "Mark",
+    score: 41
+  }
+]
+
+const btnLog = document.getElementById('btn-log')
+
+btnLog.addEventListener('click', function(){
+  console.log(data[1].score);
+})
+
+// Generate Sentence using for loop and array
+
+function generateSentence(desc, arr){
+  let baseString = `The ${arr.length} ${desc} are `
+  let lastIndex = arr.length - 1
+  for(let i = 0; i < arr.length; i++){
+    if(i === lastIndex){
+      baseString += arr[i]
+    }else{
+      baseString += arr[i] + ', '
+    }
+  }
+  return baseString;
+}
+
+const sentence = generateSentence('best fruits', ['Apple', 'Banana'])
+
+console.log(sentence);
+
+
+// Render Images
+const imgs =[
+  'img/01.png',
+  'img/02.jpg',
+  'img/03.png'
+]
+
+const imgLists = document.getElementById('image-lists');
+
+function generateImg(){
+  let images = '';
+  for(let i = 0; i < imgs.length; i++){
+      images +=  `<img alt="Family" src="${imgs[i]}" />`
+  }
+  imgLists.innerHTML = images
+}
+
+generateImg();
+
+
+
+
+
+
